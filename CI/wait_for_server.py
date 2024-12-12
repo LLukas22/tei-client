40a4c93dfd6ca6c949f7351ef5f7e50417437c80
Lukas Kreussel <lukaskreussel@gmail.com>
2024-12-12 13:53:15 +0100

    formatting

--- CI/wait_for_server.py
+++ CI/wait_for_server.py
@@ -1,17 +1,17 @@
-from tei_client import HttpClient,GrpcClient
+from tei_client import HttpClient, GrpcClient
 import time
 
 
-HTTP_URLS = ['http://localhost:8080', 'http://localhost:8082', 'http://localhost:8086']
-GRPC_URLS = ['localhost:8081', 'localhost:8083', 'localhost:8085']
+HTTP_URLS = ["http://localhost:8080", "http://localhost:8082", "http://localhost:8086"]
+GRPC_URLS = ["localhost:8081", "localhost:8083", "localhost:8085"]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	print("Waiting docker healtchecks ...")
 	while len(HTTP_URLS) > 0:
 		for url in HTTP_URLS:
 			client = HttpClient(url)
 			if client.health():
-				print('http', url, 'is healthy')
+				print("http", url, "is healthy")
 				HTTP_URLS.remove(url)
 		time.sleep(2)
 
@@ -19,9 +19,8 @@ if __name__ == '__main__':
 		for url in GRPC_URLS:
 			client = GrpcClient(url)
 			if client.health():
-				print('grpc', url, 'is healthy')
+				print("grpc", url, "is healthy")
 				GRPC_URLS.remove(url)
 		time.sleep(2)
-	
-	print('all services are healthy')
-	
+
+	print("all services are healthy")
